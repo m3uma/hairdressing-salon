1e2ac746c4f06251f8f613f403ebe36fc3047ba0
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-09-09 16:25:12 +0200

    add some comments

--- js/main.js
+++ js/main.js
@@ -8,4 +8,8 @@
     document.querySelector(".form__button--close").addEventListener("click", () => {
         document.querySelector(".form").style.display = "none";
     })
+
+    //document.querySelector("html").style.minHeight = window.innerHeight;
+    //document.querySelector("body").style.minHeight = window.innerHeight;
+
 }
