9dcc792f3c12f79b73d89427a450a726ac03551e
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-09-09 15:24:32 +0200

    add show and close form appointment

---
+++ js/main.js
@@ -0,0 +1,11 @@
+{
+    const appointmentButton = document.querySelector(".button");
+
+    appointmentButton.addEventListener("click", () => {
+        document.querySelector(".form").style.display = "block";
+    })
+
+    document.querySelector(".form__button--close").addEventListener("click", () => {
+        document.querySelector(".form").style.display = "none";
+    })
+}
