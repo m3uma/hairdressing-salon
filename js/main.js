9c29e72eb4b89414efa817c486b53c34108f8555
Magdalena Czuma <perfect.stranger1830@gmail.com>
2021-09-14 17:54:58 +0200

    set min date in input date

--- js/main.js
+++ js/main.js
@@ -9,4 +9,16 @@
         document.querySelector(".form").style.display = "none";
     })
 
+    $(function(){
+        var date = new Date();
+        var day = date.getDate() + 1;
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        if (day < 10) day = "0" + day;
+        if (month < 10) month = "0" + month;
+        var tomorrow = year + "-" + month + "-" + day;
+
+        $(".form__input--date").attr('min', tomorrow);
+    })
+
 }
